3b23a966fc9bbf1368470f9382ca3b0173fd5d66
Ayush Kumar <ayushtiwari0803@gmail.com>
2024-07-26 12:54:29 +0530

    All Code Added for the first time

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.html"],
+
+  theme: {
+    fontFamily : {
+      'ss': ['sans-serif', 'Ostwald']
+    },
+    extend: {},
+  },
+  plugins: [],
+};
